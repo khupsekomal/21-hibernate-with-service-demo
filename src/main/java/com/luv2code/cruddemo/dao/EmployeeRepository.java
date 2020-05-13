061f018987c50fc9835b5d764831e96b4373ee76
komal <khupsekomal@gmail.com>
2020-05-13 17:22:33 +0530

    crudedemo for implementation with spring-data-rest

--- src/main/java/com/luv2code/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/luv2code/cruddemo/dao/EmployeeRepository.java
@@ -2,9 +2,11 @@ package com.luv2code.cruddemo.dao;
 
 import com.luv2code.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import java.util.List;
+import javax.persistence.EntityResult;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 
 }
